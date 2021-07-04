89a6c950a838aacfa114f2211fe38e13c0c32638
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-04 17:46:05 -0400

    method to embed components to theis parents and change templete and style

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -5,10 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
+
+// template: '<div>Hello World</div>'
+// style: `
+// div {
+//   color: red
+// }
+// `
+
 export class TestComponent implements OnInit {
 
   constructor() { }
 
+
+  // A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
+  // Define an ngOnInit() method to handle any additional initialization tasks.
   ngOnInit(): void {
   }
 
