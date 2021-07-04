a43a2371de72dad66204d17880981273ac70c6d9
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-04 17:34:46 -0400

    add comments and generate a child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+// component decorator
+// selector: <app-root></app-root>
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// class
 export class AppComponent {
   title = 'angular-tutorial';
 }
