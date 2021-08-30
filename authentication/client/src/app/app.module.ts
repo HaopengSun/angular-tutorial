add1957fe3712db85e1285ffb615ba535a14a7d6
HaopengSun <sunhaopeng2016@gmail.com>
2021-08-29 22:37:36 -0400

    add components and boostrap

--- authentication/client/src/app/app.module.ts
+++ authentication/client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { EventsComponent } from './components/events/events.component';
+import { SpecialEventsComponent } from './components/special-events/special-events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    EventsComponent,
+    SpecialEventsComponent
   ],
   imports: [
     BrowserModule,
